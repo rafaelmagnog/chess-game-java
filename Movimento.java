5195cd8f00d3e5ae7cfd2e956dc1a2240227a420
HugoLinsX <hugolins144@gmail.com>
2025-06-08 20:45:09 -0300

    Primeiro commit

---
+++ Movimento.java
@@ -0,0 +1,22 @@
+// Movimento.java
+package br.edu.seuProjeto.xadrez;
+
+public class Movimento {
+    private Posicao origem, destino;
+    private Peca peca, pecaCapturada;
+
+    public Movimento(Peca peca, Posicao origem, Posicao destino) {
+        this.peca = peca;
+        this.origem = origem;
+        this.destino = destino;
+    }
+
+    /** retorna true se o movimento foi realizado */
+    public boolean executar(Tabuleiro tab) {
+        if (!peca.validarMovimento(destino, tab)) return false;
+        Casa dest = tab.getCasa(destino);
+        pecaCapturada = dest.getPeca();
+        peca.mover(destino, tab);
+        return true;
+    }
+}
