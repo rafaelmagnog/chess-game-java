55b625b90e70e79bbd9d6c94e92de7285d0508dc
jose henrique <contatojosehmelo@gmail.com>
2025-06-09 06:34:03 -0300

    Adcionadas as classes tabuleiro e casa

--- src/main/chess/Casa.java
+++ src/main/chess/Casa.java
@@ -1,4 +1,32 @@
 package main.chess;
 
 public class Casa {
+    private Posicao posicao;
+    private Peca peca; // pode ser null
+
+    public Casa(Posicao posicao) {
+        this.posicao = posicao;
+    }
+    public boolean isOccupied() {
+        return peca != null;
+    }
+    public Peca getPeca() {
+        return peca;
+    }
+    public void setPeca(Peca p) {
+        this.peca = p;
+        if (p != null) p.setCasaAtual(this);
+    }
+    public void removePeca() {
+        this.peca = null;
+    }
+    public Posicao getPosicao() {
+        return posicao;
+    }
+    @Override
+    public String toString() {
+        return isOccupied() ? peca.toString() : "--";
+    }
 }
+
+
--- src/main/chess/Tabuleiro.java
+++ src/main/chess/Tabuleiro.java
@@ -1,4 +1,48 @@
 package main.chess;
 
+import java.util.*;
+
 public class Tabuleiro {
+    private Casa[][] casas = new Casa[8][8];
+
+    public Tabuleiro() {
+        construirTabuleiro();
+        iniciarTabuleiro();
+    }
+
+    /** cria as 64 casas */
+    public void construirTabuleiro() {
+        for (int i = 1; i <= 8; i++) {
+            for (char c = 'a'; c <= 'h'; c++) {
+                casas[i-1][c-'a'] = new Casa(new Posicao(i, c));
+            }
+        }
+    }
+
+    /** posiciona as peças nas casas iniciais */
+    public void iniciarTabuleiro() {
+        // jogadores criarão as peças após instanciar o tabuleiro
+    }
+
+    public Casa getCasa(Posicao pos) {
+        return casas[pos.rowIndex()][pos.colIndex()];
+    }
+
+    /** return true se todas as casas entre orig e destino estao vazias */
+    public boolean isCaminhoLivre(Posicao orig, Posicao dest) {
+        int dr = Integer.signum(dest.getLinha() - orig.getLinha());
+        int dc = Integer.signum(dest.getColuna() - orig.getColuna());
+        int r = orig.rowIndex() + dr, c = orig.colIndex() + dc;
+        while (r != dest.rowIndex() || c != dest.colIndex()) {
+            if (casas[r][c].isOccupied()) return false;
+            r += dr; c += dc;
+        }
+        return true;
+    }
+
+    /** testa se após o mov, o rei do mov.peca fica em xeque */
+    public boolean testaMovimentoXeque(Movimento mov) {
+        // para simplificar, não implementado detalhe de simulação
+        return false;
+    }
 }
