b6a3a614b81cd385d09763ba4e55471e62da0381
Levi Adler <levialbuquerque05@gmail.com>
2025-06-08 21:50:04 -0300

    Update Jogador.java
    Criando jogador.java - atualização 1.0

--- src/main/chess/Jogador.java
+++ src/main/chess/Jogador.java
@@ -1,4 +1,26 @@
 package main.chess;
 
+import java.util.*;
+
 public class Jogador {
+    private String nome;
+    private Cor cor;
+    private List<Peca> pecas = new ArrayList<>();
+
+    public Jogador(String nome, Cor cor) {
+        this.nome = nome;
+        this.cor = cor;
+    }
+
+    public String getNome() { return nome; }
+    public Cor getCor() { return cor; }
+    public List<Peca> getPecas() { return pecas; }
+
+    void addPeca(Peca p) { pecas.add(p); }
+    public void removerPeca(Peca p) { pecas.remove(p); }
+
+    /* só resta o rei? então perdeu! */
+    public boolean temSomenteRei() {
+        return pecas.stream().allMatch(p -> p instanceof Rei);
+    }
 }
